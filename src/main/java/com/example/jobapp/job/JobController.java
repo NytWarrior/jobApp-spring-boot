1e0bf0fa0d366772af02ebf69f6546385dfd11d4
Rajeev <rajeevkr8917@gmail.com>
2024-04-12 18:19:02 +0530

    service fot jobs

--- src/main/java/com/example/jobapp/job/JobController.java
+++ src/main/java/com/example/jobapp/job/JobController.java
@@ -1,25 +1,38 @@
 package com.example.jobapp.job;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JobController {
-    private List<Job> jobs = new ArrayList<>();
+    private JobService jobService;
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
 
     @GetMapping("/jobs")
-    public List<Job> getJobs() {
-        return jobs;
+    public List<Job> findAll() {
+        return jobService.findAll();
     }
 
     @PostMapping("/jobs")
     public String addJob(@RequestBody Job job) {
-        jobs.add(job);
+        jobService.createJob(job);
         return "Job added successfully";
+    }
+
+    @GetMapping("/jobs/{id}")
+    public Job getJobById(@PathVariable Long id) {
+        Job job = jobService.getJobById(id);
+        if (job != null) {
+            return job;
+        }
+        return new Job(1L, "Job not found", "Job not found", "Job not found", null, null);
     }
 }
